1e5ae3632634f346792ff05221a92267fb1ed7fe
ImClod <84680532+ImClod@users.noreply.github.com>
2021-06-15 19:11:48 +0200

    Update fxmanifest.lua

--- smontaarmi/fxmanifest.lua
+++ smontaarmi/fxmanifest.lua
@@ -2,7 +2,11 @@ fx_version "bodacious"
 game "gta5"
 
 
-client_script {"project/client/MxM_C.lua","client/main.lua"}
+client_script {
+    "config.lua",
+    "project/client/MxM_C.lua",
+    "client/main.lua"
+}
 server_script {
     "config.lua",
     'project/server/MxM_S.lua',
